a6ceadb6ae656a00aac3f1d3e09b005a7e08598b
Matthew <matthewcrews@gmail.com>
2021-08-22 11:46:19 -0700

    fix

--- HadamardProductSIMD/Program.fs
+++ HadamardProductSIMD/Program.fs
@@ -9,56 +9,56 @@ let aMaxIndex = 10_000
 let bIndexCount = 1_000
 let bMaxIndex = 10_000
 
-let aIndices =
+let aKeys =
     let x = 
         [|for _ in 1 .. aIndexCount -> rng.Next (0, aMaxIndex)|]
         |> Array.distinct
     Array.sortInPlace x
     x
 
-let bIndices =
+let bKeys =
     let x = 
         [|for _ in 1 .. bIndexCount -> rng.Next (0, bMaxIndex)|]
         |> Array.distinct
     Array.sortInPlace x
     x
 
-let aValues = [| for _ in 1 .. aIndices.Length -> rng.NextDouble () |]
-let bValues = [| for _ in 1 .. bIndices.Length -> rng.NextDouble () |]
+let aValues = [| for _ in 1 .. aKeys.Length -> rng.NextDouble () |]
+let bValues = [| for _ in 1 .. bKeys.Length -> rng.NextDouble () |]
 
-let hadamardProduct (aIndices: Span<int>, aValues: Span<float>, bIndices: Span<int>, bValues: Span<float>) =
-    let maxN = Math.Max (aIndices.Length, bIndices.Length)
-    let outIndices = Array.zeroCreate maxN
+let hadamardProduct (aKeys: Span<int>, aValues: Span<float>, bKeys: Span<int>, bValues: Span<float>) =
+    let maxN = Math.Min (aKeys.Length, bKeys.Length)
+    let outKeys = Array.zeroCreate maxN
     let outValues = Array.zeroCreate maxN
 
     let mutable aIdx = 0
     let mutable bIdx = 0
     let mutable outIdx = 0
 
-    while aIdx < aIndices.Length && bIdx < bIndices.Length do
+    while aIdx < aKeys.Length && bIdx < bKeys.Length do
         
-        if aIndices.[aIdx] = bIndices.[bIdx] then
-            outIndices.[outIdx] <- aIndices.[aIdx]
+        if aKeys.[aIdx] = bKeys.[bIdx] then
+            outKeys.[outIdx] <- aKeys.[aIdx]
             outValues.[outIdx] <- aValues.[aIdx] * bValues.[bIdx]
             outIdx <- outIdx + 1
             aIdx <- aIdx + 1
             bIdx <- bIdx + 1
-        elif aIndices.[aIdx] < bIndices.[bIdx] then
+        elif aKeys.[aIdx] < bKeys.[bIdx] then
             aIdx <- aIdx + 1
         else
             bIdx <- bIdx + 1
 
-    let resultIndices = Memory (outIndices, 0, outIdx)
+    let resultKeys = Memory (outKeys, 0, outIdx)
     let resultValues = Memory (outValues, 0, outIdx)
 
-    resultIndices, resultValues
+    resultKeys, resultValues
 
 
 type Benchmarks () =
 
     [<Benchmark>]
     member _.Test () =
-        hadamardProduct (aIndices.AsSpan(), aValues.AsSpan(), bIndices.AsSpan(), bValues.AsSpan())
+        hadamardProduct (aKeys.AsSpan(), aValues.AsSpan(), bKeys.AsSpan(), bValues.AsSpan())
 
 [<EntryPoint>]
 let main argv =
